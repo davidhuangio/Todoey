357f792d75e630dc402aa91dbd44a7147bcf5ea6
David Huang <huang.z.david@gmail.com>
2018-08-09 15:37:26 -0700

    Added ability to add new items

--- Todoey/ToDoListController.swift
+++ Todoey/ToDoListController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListController : UITableViewController {
     
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,6 +43,34 @@ class ToDoListController : UITableViewController {
             selectedCell.accessoryType = selectedCell.accessoryType == .checkmark ? .none : .checkmark
         }
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //IBActions
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add To Do Item", message: "", preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "To Do Item"
+        }
+        alert.addAction(UIAlertAction(title: "Add Item", style: .default, handler: { (action) in
+            if let item = alert.textFields![0].text, !item.isEmpty{
+                self.tableView.beginUpdates()
+                self.itemArray.append(item)
+                self.tableView.insertRows(at: [IndexPath(row: self.itemArray.count - 1, section: 0)], with: UITableViewRowAnimation.automatic)
+                self.tableView.endUpdates()
+                self.tableView.reloadD
+            }else{
+                let emptyAlert = UIAlertController(title: "Error", message: "Cannot add an empty item!", preferredStyle: .alert)
+                let when = DispatchTime.now() + 1
+                DispatchQueue.main.asyncAfter(deadline: when, execute: {
+                    emptyAlert.dismiss(animated: true, completion: {
+                        self.present(alert, animated: true, completion: nil)
+                    })
+                })
+               self.present(emptyAlert, animated: true, completion: nil)
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 }
 
